645b128321dbc4dcd5690a6ed0da544a10df9634
wandumi <munandih@gmail.com>
2021-09-02 15:55:59 +0200

    Adding the shop view

--- functions.php
+++ functions.php
@@ -40,6 +40,23 @@
                 'simply_stickit_footer_menu' => 'Simply Stickit Footer Menu'
             )
         );
+
+        add_theme_support( 'woocommerce', array(
+            'thumbnail_image_width' => 255,
+            'single_image_width' => 255,
+            'product_grid' => array(
+                'default_rows' => 2,
+                'min_rows' => 1,
+                'max_rows' => 10,
+                'default_columns' => 2,
+                'min_columns' => 1,
+                'max_columns' => 4
+            )
+        ) );
+
+        add_theme_support( 'wc-product-gallery-zoom' );
+        add_theme_support( 'wc-product-gallery-lightbox' );
+        add_theme_support( 'wc-product-gallery-slider' );
     }
 
     add_action( 'after_setup_theme', 'simply_stickit_config');
