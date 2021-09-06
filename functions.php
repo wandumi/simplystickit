28a9a73a4cb88b891b93081aa4ff845c7318edb5
wandumi <munandih@gmail.com>
2021-09-06 15:46:25 +0200

    Header, login, myaccount, cartsvg, added

--- functions.php
+++ functions.php
@@ -14,11 +14,23 @@
      */
 
     
+    /**
+     * Register Custom Navigation Walker
+     */
+    function register_navwalker(){
+        require_once get_template_directory() . '/inc/class-wp-bootstrap-navwalker.php';
+    }
+    
+    add_action( 'after_setup_theme', 'register_navwalker' );
+    
     function simply_stickit_scripts()
     {
         wp_enqueue_script( 'main-js', get_template_directory_uri() . '/inc/js/bootstrap.min.js', array( 'jquery' ), '4.6.0', true );
         wp_enqueue_style( 'main-css', get_template_directory_uri() . '/inc/css/bootstrap.min.css', array(), '4.6.0', 'all' );
 
+        wp_enqueue_style( 'simply-stickit-fonts', get_stylesheet_uri(), 
+            'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet 
+            | https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet');
         // Theme main style
         wp_enqueue_style( 'simply-stickit-style', get_stylesheet_uri(), array(), filemtime( get_template_directory().'/inc/css/style.css' ), 'all' );
     }
@@ -38,9 +50,10 @@
          */
         register_nav_menus(
             array(
-                'simply_stickit_main_menu' => 'Simply Stickit Main Menu',
-                'simply_stickit_top_menu'  => 'Simply Stickit Top Menu',
-                'simply_stickit_footer_menu' => 'Simply Stickit Footer Menu'
+                // 'simply_stickit_main_menu' => 'Simply Stickit Main Menu',
+                // 'simply_stickit_top_menu'  => 'Simply Stickit Top Menu',
+                // 'simply_stickit_footer_menu' => 'Simply Stickit Footer Menu'
+                'primary' => __( 'simply_stickit_main_menu', 'Simply Stickit Main Menu' ),
             )
         );
 
@@ -77,4 +90,21 @@
     if( class_exists('WooCommerce' ))
     {
         require get_template_directory() . '/inc/woocommerce/wc-modifications.php';
+    }
+
+    /**
+     * Show cart contents / total Ajax
+     */
+    add_filter( 'woocommerce_add_to_cart_fragments', 'simply_stickit_woocommerce_header_add_to_cart_fragment' );
+
+    function simply_stickit_woocommerce_header_add_to_cart_fragment( $fragments ) {
+        global $woocommerce;
+
+        ob_start();
+
+        ?>
+        <span class="items"><?php echo WC()->cart->get_cart_contents_count() ?></span>
+        <?php
+        $fragments['span.items'] = ob_get_clean();
+        return $fragments;
     }
--- header.php
+++ header.php
@@ -22,37 +22,81 @@
 <body <?php body_class(); ?> >
     <div id="page" class="site">
         <header>
-            <section class="search container mt-5">
+            <!-- <section class="search">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-md-4"></div>
-                        <div class="col-md-4">
-                            <form action="" method="post">
-                                <div class="form-control">
-                                    <label for="search">Search</label>
-                                    <input type="search" name="search" id="search">
-                                </div>
-                            </form>
-                        </div>
-                        <div class="col-md-4"></div>
+                    <div class="text-center d-md-flex align-items-center">
+                        <?php /*get_search_form(); */?>
+
                     </div>
                 </div>
-            </section>
+            </section> -->
             <section class="top-bar mt-4">
                 <div class="container">
                     <div class="row">
-                        <div class="brand col-3">Logo</div>
-                        <div class="main-menu col-6">
-                            <?php
-                                wp_nav_menu(
-                                    array(
-                                        'theme_location' => 'simply_stickit_main_menu'
-                                    )
-                                );
-                            ?>
+                        <div class="brand col-md-3 col-12 col-lg-2 text-center text-md-left">Logo</div>
+                        <div class="second-column col-md-9 col-12 col-lg-10">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="navbar-expand">
+                                        <ul class="navbar-nav float-left">
+                                            <?php if( is_user_logged_in() ) : ?>
+                                                <li >
+                                                    <a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id'))   ); ?>" class="nav-link">My Account</a>
+                                                </li>
+                                                <li >
+                                                    <a href="<?php echo esc_url(  wp_logout_url( get_permalink( get_option( 'woocommerce_myaccount_page_id'))  )   ); ?>" class="nav-link">Logout</a>
+                                                </li>
+                                            <?php else: ?>
+                                                <li >
+                                                    <a href="<?php echo esc_url( get_permalink( get_option( 'woocommerce_myaccount_page_id'))   ); ?>" class="nav-link">Login / Register</a>
+                                                </li>
+                                            <?php endif ?>
+                                        </ul>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-9">
+                                            <nav class="main_menu navbar navbar-expand-md navbar-light" role="navigation">
+                                                
+                                                    <!-- Brand and toggle get grouped for better mobile display -->
+                                                    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
+                                                        <span class="navbar-toggler-icon"></span>
+                                                    </button>
+                                                
+                                                        <?php
+                                                            wp_nav_menu( array(
+                                                                'theme_location'    => 'simply_stickit_main_menu',
+                                                                'depth'             => 3, //levels of the menu dropdowns
+                                                                'container'         => 'div',
+                                                                'container_class'   => 'collapse navbar-collapse',
+                                                                'container_id'      => 'bs-example-navbar-collapse-1',
+                                                                'menu_class'        => 'nav navbar-nav',
+                                                                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                                                                'walker'            => new WP_Bootstrap_Navwalker(),
+                                                            ) );
+                                                        ?>
+                                              
+                                            </nav>
+    
+                                        </div>
+                                        <div class="account col-3 ">
+                                            <div class="cart text-right">
+                                                <a href="<?php echo wc_get_cart_url() ?>"><span class="cart-icon"></span></a>
+                                                <span class="items"><?php echo WC()->cart->get_cart_contents_count() ?></span>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
-                        <div class="account col-3 text-md-right text-center">SignIn / SignOut</div>
+                        
                     </div>
                 </div>
             </section>
         </header>
+
+        
